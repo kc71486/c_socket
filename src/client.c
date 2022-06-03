4184f737540a9b2e489ab408e4491fd09ab19a90
kc71486 <linda20030102@gmail.com>
2022-06-03 19:57:31 +0800

    add client multithread

--- src/client.c
+++ src/client.c
@@ -2,13 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 
+const char *ADDRESS = "127.0.0.1";
+const char *NICKNAME = "client1";
+
+char pre_message[] = {"client says: "};
+char message[100];
+char receiveMessage[100] = {};
+char receiveMessage2[100] = {};
+
+void send_func() {
+    scanf("%s", message);
+    fflush(stdin);
+    send(sockfd,pre_message,sizeof(pre_message),0);
+    send(sockfd,message,sizeof(message),0);
+}
+
+void recv_func() {
+    recv(sockfd,receiveMessage,sizeof(receiveMessage),0);
+    recv(sockfd,receiveMessage2,sizeof(receiveMessage2),0);
+     printf("%s%s\n",receiveMessage, receiveMessage2);
+}
 
 int main(int argc , char *argv[]) {
-    //socket的建立
+    //create socket
     int sockfd = 0;    
     sockfd = socket(AF_INET , SOCK_STREAM , 0);
 
@@ -16,39 +38,44 @@ int main(int argc , char *argv[]) {
         printf("Fail to create a socket.");
     }
 
-    //socket的連線
-
-    struct sockaddr_in info;
-    bzero(&info,sizeof(info));
-    info.sin_family = PF_INET;
-
-    //localhost test
-    info.sin_addr.s_addr = inet_addr("127.0.0.1");
-    info.sin_port = htons(8700);
-
-
-    int err = connect(sockfd,(struct sockaddr *)&info,sizeof(info));
-    if(err==-1){
+    //socket setting
+    struct sockaddr_in server_addr, client_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    memset(&client_addr, 0, sizeof(client_addr));
+    
+    server_addr.sin_family = PF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(ADDRESS);
+    server_addr.sin_port = htons(8700);
+    
+    //connect to socket
+    int err = connect(sockfd,(struct sockaddr *)&server_addr,sizeof(server_addr))
+    if(err == -1){
         printf("Connection error");
     }
-
-
-    char pre_message[] = {"client says: "};
-    char message[100];
-    char receiveMessage[100] = {};
-    char receiveMessage2[100] = {};
     
-    while(1) {
-        //Send message to server
-        scanf("%s", message);
-        fflush(stdin);
-        send(sockfd,pre_message,sizeof(pre_message),0);
-        recv(sockfd,receiveMessage,sizeof(receiveMessage),0);
-        send(sockfd,message,sizeof(message),0);
-        recv(sockfd,receiveMessage2,sizeof(receiveMessage2),0);
-        printf("%s%s\n",receiveMessage, receiveMessage2);
+    //get name
+    getpeername(sockfd, (struct sockaddr*)server_addr, sizeof(server_addr));
+    getsockname(sockfd, (struct sockaddr*)client_addr, sizeof(client_addr));
+    printf("connect to server %s:%d\n", inet_ntoa(server_addr.sin_addr), ntohs(server_addr.sin_port));
+    printf("you are %s your ip is%s:%d\n", NICKNAME, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+
+    send(sockfd, NICKNAME, sizeof(NICKNAME), 0);
+    
+    pthread_t send_thread, recv_thread;
+    if(pthread_create(&send_thread, NULL, (void *)send_func, NULL) != 0) {
+        printf("thread creation error");
+        exit(-1);
     }
-    printf("close Socket\n");
+    if(pthread_create(&recv_thread, NULL, (void *)recv_func, NULL) != 0) {
+        printf("thread creation error");
+        exit(-1);
+    }
+    while(1) {
+        if(0) {
+            printf("close Socket\n");
+            break;
+        }
+    }
     close(sockfd);
     return 0;
 }
